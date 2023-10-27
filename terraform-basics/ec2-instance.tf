d8d39d04fb0f9d822f6e230635e7e112866c8ccf
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-27 13:43:35 +0700

    update

--- terraform-basics/ec2-instance.tf
+++ terraform-basics/ec2-instance.tf
@@ -15,4 +15,7 @@ resource "aws_instance" "ec2demo" {
     ami = "ami-06018068a18569ff2"
     instance_type = "t2.micro"
     subnet_id = "subnet-04c454d12b060d866"
+    tags = {
+      Name = "ec2-demo"
+    }
 }
