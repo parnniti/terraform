969a98dbdc6f3432b5fd64001fd4141a84e51398
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-27 17:38:02 +0700

    update

--- terraform-basics/02-input-variables/main.tf
+++ terraform-basics/02-input-variables/main.tf
@@ -16,7 +16,6 @@ resource "aws_instance" "ec2demo" {
   ami = "ami-06018068a18569ff2"
   instance_type = var.ec2_instance_type
   subnet_id = "subnet-04c454d12b060d866"
-  user_data = file("${path.module}/script.sh")
   tags = {
     Name = "ec2demo"
   }
