70a1b41d714a29e84d63b04390cfd1e8511d473f
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-27 17:36:45 +0700

    update

---
+++ terraform-basics/02-input-variables/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "> 5.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.aws_profile
+  region = var.aws_region
+}
+
+resource "aws_instance" "ec2demo" {
+  ami = "ami-06018068a18569ff2"
+  instance_type = var.ec2_instance_type
+  subnet_id = "subnet-04c454d12b060d866"
+  user_data = file("${path.module}/script.sh")
+  tags = {
+    Name = "ec2demo"
+  }
+}
