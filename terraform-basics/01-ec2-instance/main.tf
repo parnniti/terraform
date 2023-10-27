d77233c452d798191e2de8e2f4296c0b3fa295ef
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-27 17:05:17 +0700

    update

--- terraform-basics/01-ec2-instance/main.tf
+++ terraform-basics/01-ec2-instance/main.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "ec2demo" {
   ami = "ami-06018068a18569ff2"
   instance_type = "t2.medium"
   subnet_id = "subnet-04c454d12b060d866"
+  user_data = file("${path.module}/script.sh")
   tags = {
     Name = "ec2-demo"
   }
