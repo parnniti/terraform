19070b07db92eb0d9673444d9d8da5f9192133a6
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-30 15:18:00 +0700

    update

--- 05-loops/foreach/output.tf
+++ 05-loops/foreach/output.tf
@@ -8,23 +8,23 @@
 #   description = "EC2 instance public dns"
 # }
 
-# OUTPUT For Loop - with List
-output "for_output_list" {
-    description = "For Loop with List"
-    value = [for instance in aws_instance.ec2demo: instance.private_ip]
-}
+# # OUTPUT For Loop - with List
+# output "for_output_list" {
+#     description = "For Loop with List"
+#     value = [for instance in aws_instance.ec2demo: instance.private_ip]
+# }
 
-# OUTPUT For Loop - with Map
-output "for_output_map" {
-    description = "For Loop with Map"
-    value = {for instance in aws_instance.ec2demo: instance.id => instance.private_ip}
-}
+# # OUTPUT For Loop - with Map
+# output "for_output_map" {
+#     description = "For Loop with Map"
+#     value = {for instance in aws_instance.ec2demo: instance.id => instance.private_ip}
+# }
 
-# OUTPUT For Loop - with Map
-output "for_output_map2" {
-    description = "For Loop with Map - Advanced"
-    value = {for c, instance in aws_instance.ec2demo: c => instance.private_ip}
-}
+# # OUTPUT For Loop - with Map
+# output "for_output_map2" {
+#     description = "For Loop with Map - Advanced"
+#     value = {for c, instance in aws_instance.ec2demo: c => instance.private_ip}
+# }
 
 # OUTPUT for Loop - with Splat operator
 output "splat_output" {
