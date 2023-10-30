3bb6b734cf0ab5ecec10d8362509fa3eb6df5fea
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-30 15:43:14 +0700

    update

--- 05-loops/utility-project/get-instance-in-zone.tf
+++ 05-loops/utility-project/get-instance-in-zone.tf
@@ -1,24 +1,25 @@
 # Datasource
 data "aws_availability_zones" "myaz" {
-  state = "available"
-  filter {
-    name = "region-name"
-    values = [var.aws_region]
-  }
+    state = "available"
+    filter {
+        name = "region-name"
+        values = [var.aws_region]
+    }
 }
 
-data "aws_ec2_instance_type_offerings" "my_instance_type1" {
-  filter {
-    name   = "instance-type"
-    values = var.ec2_instance_type_list
-  }
+data "aws_ec2_instance_type_offerings" "my_instance_types" {
+    for_each = toset(data.aws_availability_zones.myaz.names)
+    filter {
+        name   = "instance-type"
+        values = var.ec2_instance_type_list
+    }
 
-  filter {
-    name   = "location"
-    values = data.aws_availability_zones.myaz.names
-  }
+    filter {
+        name   = "location"
+        values = each.key
+    }
 
-  location_type = "availability-zone"
+    location_type = "availability-zone"
 }
 
 # Output
@@ -26,6 +27,6 @@ output "output_az" {
     value = data.aws_availability_zones.myaz.names
 }
 
-output "output_v1_1" {
-    value = toset(data.aws_ec2_instance_type_offerings.my_instance_type1.instance_types)
+output "output_instance_types" {
+    value = {for az, instance_type in data.aws_ec2_instance_type_offerings.my_instance_types: az => instance_type}
 }
