bb182309d02b0782d04aae01d94a1eb3cb049a47
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-30 13:47:54 +0700

    update

--- 02-input-variables/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  value       = aws_instance.ec2demo.public_ip
-  description = "EC2 instance public ip"
-}
-
-output "instance_public_dns" {
-  value       = aws_instance.ec2demo.public_dns
-  description = "EC2 instance public dns"
-}
