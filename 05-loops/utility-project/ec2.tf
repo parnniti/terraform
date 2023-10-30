9b019cf3a4061324d9695270459ebcc91029a7b4
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-30 16:23:17 +0700

    update

---
+++ 05-loops/utility-project/ec2.tf
@@ -0,0 +1,28 @@
+data "aws_availability_zones" "myaz" {
+  state = "available"
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+resource "aws_instance" "ec2demo" {
+  subnet_id = var.ec2_subnet_id
+  ami = data.aws_ami.amzn2.id
+  # instance_type = var.ec2_instance_type # For String
+  # instance_type = var.ec2_instance_type_list[1] # For List
+  instance_type = var.ec2_instance_type_map["prod"] # For Map
+  vpc_security_group_ids = [aws_security_group.sg-ssh.id, aws_security_group.sg-web.id]
+  key_name = var.ec2_instance_keypair
+  user_data = file("${path.module}/script.sh")
+
+  for_each = toset(keys({
+    for az, data in data.aws_ec2_instance_type_offerings.my_instance_types:
+      az => data.instance_types if length(data.instance_types) != 0
+  }))
+  availability_zone = each.key
+
+  tags = {
+    Name = "ec2demo-foreach-${each.value}"
+  }
+}
