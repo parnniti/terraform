c9af971624dd8f72412fb567e16804191e012e50
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-30 14:28:14 +0700

    update

--- 05-loops/output.tf
+++ 05-loops/output.tf
@@ -7,3 +7,27 @@
 #   value       = aws_instance.ec2demo.public_dns
 #   description = "EC2 instance public dns"
 # }
+
+# OUTPUT For Loop - with List
+output "for_output_list" {
+    description = "For Loop with List"
+    value = [for instance in aws_instance.ec2demo: instance.private_ip]
+}
+
+# OUTPUT For Loop - with Map
+output "for_output_map" {
+    description = "For Loop with Map"
+    value = [for instance in aws_instance.ec2demo: instance.id => instance.private_ip]
+}
+
+# OUTPUT For Loop - with Map
+output "for_output_map2" {
+    description = "For Loop with Map - Advanced"
+    value = {for c, instance in aws_instance.ec2demo: c => instance.private_ip}
+}
+
+# OUTPUT for Loop - with Splat operator
+output "splat_output" {
+    description = "For Loop with Map - Advanced"
+    value = aws_instance.ec2demo[*].private_ip
+}
