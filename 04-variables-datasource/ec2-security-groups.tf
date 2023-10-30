64d2468e48845d347224de2b7f302e3140b20a7e
parnniti <98480500+parnniti@users.noreply.github.com>
2023-10-30 13:43:58 +0700

    update

---
+++ 04-variables-datasource/ec2-security-groups.tf
@@ -0,0 +1,57 @@
+resource "aws_security_group" "sg-ssh" {
+  description = "Allow ingress port 22"
+
+  ingress {
+    description      = "Allow Port 22"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description      = "Allow all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sg-ssh"
+  }
+}
+
+resource "aws_security_group" "sg-web" {
+  description = "Allow ingress application ports"
+
+  ingress {
+    description      = "Allow Port 80"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Allow Port 443"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description      = "Allow all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sg-web"
+  }
+}
