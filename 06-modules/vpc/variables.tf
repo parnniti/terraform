8899dc7edb757cea65cc78f5f37f0d0c936893aa
parnniti <98480500+parnniti@users.noreply.github.com>
2023-11-01 15:11:45 +0700

    update

---
+++ 06-modules/vpc/variables.tf
@@ -0,0 +1,11 @@
+variable aws_region {
+  type        = string
+  default     = "ap-southeast-1"
+  description = "AWS region"
+}
+
+variable aws_profile {
+  type        = string
+  default     = "ChaiyoPowerUserRole"
+  description = "AWS Profile"
+}
